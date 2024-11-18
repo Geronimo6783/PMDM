ca7b17e686c31cc186bcf7e9f64208ed701ef4fa
Lois Domínguez Domínguez <stilton1267@gmail.com>
2024-11-18 21:51:00 +0100

    UD003EJ011J terminado.

---
+++ Android/Java/UD3/UD003EJ011J/app/src/main/java/com/pmdm/ud003ej011j/Actividad2.java
@@ -0,0 +1,76 @@
+package com.pmdm.ud003ej011j;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Actividad2 extends AppCompatActivity {
+
+    private int numeroCambios;
+
+    private Button boton1;
+
+    private Button boton2;
+
+    private Button boton3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Bundle extras = getIntent().getExtras();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.actividad2);
+        TextView panelPantallaAnterior = findViewById(R.id.panelPantallaAnterior);
+        String textoPanelPantallaAnterior = "Pantalla anterior " + ((extras != null) ? extras.getInt("Actividad Anterior") : "");
+        panelPantallaAnterior.setText(textoPanelPantallaAnterior);
+        numeroCambios = (extras != null) ? extras.getInt("Número Cambios") : 0;
+        TextView panelNumeroCambios = findViewById(R.id.numeroCambios);
+        panelNumeroCambios.setText(String.format("%d", numeroCambios));
+        boton1 = findViewById(R.id.Button01);
+        boton2 = findViewById(R.id.Button02);
+        boton3 = findViewById(R.id.Button03);
+
+        boton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                irAActividad1(null);
+            }
+        });
+
+        boton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                irAActividad2(null);
+            }
+        });
+
+        boton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                irAActividad3(null);
+            }
+        });
+    }
+
+    private void irAActividad1(View view){
+        Intent i = new Intent(this, MainActivity.class);
+        i.putExtra("Número Cambios", ++numeroCambios);
+        i.putExtra("Actividad Anterior", 2);
+        startActivity(i);
+    }
+
+    private void irAActividad2(View view){
+        Intent i = new Intent(this, Actividad2.class);
+        i.putExtra("Número Cambios", ++numeroCambios);
+        i.putExtra("Actividad Anterior", 2);
+        startActivity(i);
+    }
+
+    private void irAActividad3(View view){
+        Intent i = new Intent(this, Actividad3.class);
+        i.putExtra("Número Cambios", ++numeroCambios);
+        i.putExtra("Actividad Anterior", 2);
+        startActivity(i);
+    }
+}
--- Android/Java/UD3/UD003EJ011J/app/src/main/java/com/pmdm/ud003ej011j/MainActivity.java
+++ Android/Java/UD3/UD003EJ011J/app/src/main/java/com/pmdm/ud003ej011j/MainActivity.java
@@ -1,24 +1,76 @@
 package com.pmdm.ud003ej011j;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int numeroCambios;
+
+    private Button boton1;
+
+    private Button boton2;
+
+    private Button boton3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Bundle extras = getIntent().getExtras();
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        TextView panelPantallaAnterior = findViewById(R.id.panelPantallaAnterior);
+        String textoPanelPantallaAnterior = "Pantalla anterior " + ((extras != null) ? extras.getInt("Actividad Anterior") : "");
+        panelPantallaAnterior.setText(textoPanelPantallaAnterior);
+        numeroCambios = (extras != null) ? extras.getInt("Número Cambios") : 0;
+        TextView panelNumeroCambios = findViewById(R.id.numeroCambios);
+        panelNumeroCambios.setText(String.format("%d", numeroCambios));
+        boton1 = findViewById(R.id.Button01);
+        boton2 = findViewById(R.id.Button02);
+        boton3 = findViewById(R.id.Button03);
+
+        boton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                irAActividad1(null);
+            }
         });
+
+        boton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                irAActividad2(null);
+            }
+        });
+
+        boton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                irAActividad3(null);
+            }
+        });
+    }
+
+    private void irAActividad1(View view){
+        Intent i = new Intent(this, MainActivity.class);
+        i.putExtra("Número Cambios", ++numeroCambios);
+        i.putExtra("Actividad Anterior", 1);
+        startActivity(i);
+    }
+
+    private void irAActividad2(View view){
+        Intent i = new Intent(this, Actividad2.class);
+        i.putExtra("Número Cambios", ++numeroCambios);
+        i.putExtra("Actividad Anterior", 1);
+        startActivity(i);
+    }
+
+    private void irAActividad3(View view){
+        Intent i = new Intent(this, Actividad3.class);
+        i.putExtra("Número Cambios", ++numeroCambios);
+        i.putExtra("Actividad Anterior", 1);
+        startActivity(i);
     }
 }
