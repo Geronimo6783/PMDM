507c88b9ad1bf673fc0ce7f1b4858cdbb4526492
Geronimo6783 <stilton1267@gmail.com>
2024-11-19 13:03:34 +0100

    UD003EJ011K terminado.

--- Android/Kotlin/UD3/UD003EJ011K/app/build.gradle.kts
+++ Android/Kotlin/UD3/UD003EJ011K/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.pmdm.ud003ej011k"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.pmdm.ud003ej011k"
--- Android/Kotlin/UD3/UD003EJ011K/app/src/main/java/com/pmdm/ud003ej011k/Actividad2.kt
+++ Android/Kotlin/UD3/UD003EJ011K/app/src/main/java/com/pmdm/ud003ej011k/Actividad2.kt
@@ -1,7 +1,62 @@
 package com.pmdm.ud003ej011k
 
+import android.content.Intent
+import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
-class Actividad2(private val numeroCambios: Int, private val boton1: Button, private val boton2: Button, private val boton3: Button) : AppCompatActivity() {
+class Actividad2(private var numeroCambios: Int, private var boton1: Button?, private var boton2: Button?, private var boton3: Button?) : AppCompatActivity() {
+
+    constructor() : this(0, null, null, null)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        val extras: Bundle? = getIntent().getExtras()
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.actividad2)
+        val panelPantallaAnterior: TextView = findViewById(R.id.panelPantallaAnterior)
+        val textoPanelPantallaAnterior: String = "Pantalla anterior " + (if ((extras != null)) extras.getInt("Actividad Anterior") else "")
+        panelPantallaAnterior.setText(textoPanelPantallaAnterior)
+        numeroCambios = (if (extras != null) extras.getInt("Número Cambios") else 0)
+        val panelNumeroCambios: TextView = findViewById(R.id.numeroCambios)
+        panelNumeroCambios.setText(String.format("%d", numeroCambios))
+        boton1 = findViewById(R.id.Button01)
+        boton2 = findViewById(R.id.Button02)
+        boton3 = findViewById(R.id.Button03)
+
+        boton1?.setOnClickListener {
+            irAActividad1(null)
+        }
+
+        boton2?.setOnClickListener{
+            irAActividad2(null)
+        }
+
+        boton3?.setOnClickListener{
+            irAActividad3(null)
+        }
+    }
+
+    private fun irAActividad1(view: View?){
+        val i: Intent = Intent(this, MainActivity::class.java)
+        i.putExtra("Número Cambios", numeroCambios + 1)
+        i.putExtra("Actividad Anterior", 2)
+        startActivity(i)
+    }
+
+    private fun irAActividad2(view: View?){
+        val i: Intent = Intent(this, Actividad2::class.java)
+        i.putExtra("Número Cambios", numeroCambios + 1)
+        i.putExtra("Actividad Anterior", 2)
+        startActivity(i)
+    }
+
+    private fun irAActividad3(view: View?){
+        val i: Intent = Intent(this, Actividad3::class.java)
+        i.putExtra("Número Cambios", numeroCambios + 1)
+        i.putExtra("Actividad Anterior", 2)
+        startActivity(i)
+    }
 }
